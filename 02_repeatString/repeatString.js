21d4d2490a0c098ca9b709b165054f3e59137689
Pete Fowler <fowler.pete@gmail.com>
2022-05-10 12:07:25 -0600

    Finish exercise 4 up to can remove all values

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, times) {
+    let newString = "";
+    if (times == 0) {
+        return newString;
+    }
+    if (times < 0) {
+        newString = "ERROR";
+        return newString;
+    }
+    for (let i = 1; i <= times; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
 
+
+const reverseString = function(string) {
+    let length = string.length;
+    let array = [];
+
+    for (let i = string.length; i >= 0; i--) {
+        array.push(string.slice(i - 1, i));
+    }
+
+    let newString = array.join("");
+    return newString;
 };
 
 // Do not edit below this line
