66262fc7b91a02ad51e8f0c93f57ecc07c3ef6ad
Pete Fowler <fowler.pete@gmail.com>
2022-05-10 13:59:08 -0600

    Update readme in 04 to note lessons learned

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,10 @@
 /*
-Implement a function that takes an array and some other arguments then removes 
-the other arguments from that array. It will need to
-
-loop through array to search for each arg
-    for loop to go through array[0] to array.length -1
-
-remove each arg with a splice()
-    nested for loop to loop through each arg - arg[0] through arg.length - 1
-    On each loop, check if the values match, and if so, splice them out
-
-how to deal with changing array length after it is spliced?
-
-Example: removeFromArray(['hey', 2, 3, 'ho'], ['hey', 3] should return [2, 'ho'] but is removing 
-hey and then ho
-
-at i=1 j=1 it removed ho then got stuck
-
+Lessons:
+- Had to switch which for loop was nested because originally I had it skipping 
+a value. 
+- Falsely assumed I had no syntax errors and ran into later trouble because my 
+syntax error of array.splice(array[i] -1, 1) somehow managed to pass tests. So
+NEVER ASSUME YOU DON'T HAVE A SYNTAX ERROR and double check MDN docs.
 */
 
 const removeFromArray = function(array, ...args) {
