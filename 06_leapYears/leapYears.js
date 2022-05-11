6f0fad4c0b3f0720d973ecc92a904e2dbf52bee9
Pete Fowler <fowler.pete@gmail.com>
2022-05-11 06:40:58 -0600

    Finish exercise 05

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+/*
+Leap years are divisible by 4
+leap years are divisible by 100 if also divisible by 400
+*/
 
-};
-
+const leapYears = function(year) {
+    if(year % 4 !== 0) {
+        return false;
+    } else if(year % 400 !== 0 && year % 100 === 0) {
+        return false;
+    } else return true;
+}
 // Do not edit below this line
 module.exports = leapYears;
