066b5a04979dd6a6aa38fb9750f757f730c43b74
Pete Fowler <fowler.pete@gmail.com>
2022-05-11 20:55:54 -0600

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num, num2) {
+	return num + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num, num2) {
+	return num - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let num of array) {
+    total += num;
+  }
+  return total;
+};
+// could also be:
+// return array.reduce((previousVal, currentVal) => previousVal + currentVal, 0) 
+
+const multiply = function(array) {
+  return array.reduce((previous, current) => previous * current, 1);
 };
 
-const multiply = function() {
-
+const power = function(n, p) {
+  return Math.pow(n, p);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(int) {
+	let f = 1;
+  for (let i = int; i >= 1; i--) {
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
