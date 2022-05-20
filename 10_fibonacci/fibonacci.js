4e9729fd9e474f6087f4bed903a95a7c93860f58
Pete Fowler <fowler.pete@gmail.com>
2022-05-19 20:51:24 -0600

    Finish 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
 
+  
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+    return 'OOPS';
+    }
+    const sequence = [1, 1];
+
+    for (let i = 0; i < num - 2; i++) {
+        let next = sequence[i] + sequence[i+1];
+        sequence.push(next);    
+    }
+    return sequence[num - 1];
 };
+
+// recursive answer body of function :
+// if (num <= 1) return 1;
+
+// return fibonacci(num - 1) + fibonacci(num - 2);
 
 // Do not edit below this line
 module.exports = fibonacci;
