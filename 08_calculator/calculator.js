566bfcc862969d9294ac0e5d7aec427fbe7238f9
Pete Fowler <fowler.pete@gmail.com>
2022-05-19 18:26:26 -0600

    Finish 09 palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) {
+    return 0;
+  } else {
+  return array.reduce(function (prev, current) {
+    return prev + current;
+  }, 0);
+}
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, current) => prev * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+  for (let i = n; i > 1; i--) {
+    n *= i - 1;
+  }
+  return n;
+}
+  // recursive method:
+  // if (n=1) {
+  //   return 1;
+  // } else {
+  //   return n * factorial(n - 1);
+  // }
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let fwdStr = str
+    .toLowerCase()
+    .replace(/[^A-Za-z]/g, '')
+    .split('');
 
+    let bwdStr = [];
+    for (let i = 0; i < fwdStr.length; i++) {
+        bwdStr.unshift(fwdStr[i]);
+    }
+    bwdStr = bwdStr.join('');
+    fwdStr = fwdStr.join('');
+    if (bwdStr === fwdStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
